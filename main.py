2216767d75db201f17b065e6dbfb10b09635feb1
EdsonSan <erodrigues.santos@gmail.com>
2025-03-06 22:26:19 -0300

    Correção Rotina Dicionary, PATH, DELT POST e GET Titulo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from itertools import count
-from tokenize import String
+# from itertools import count
+# from tokenize import String
 from typing import Dict, List, Optional
 from fastapi import FastAPI, responses
 from fastapi import HTTPException
@@ -10,6 +10,7 @@ import util
 from models import AlteraCurso, Curso
 
 global dbcursos_json
+global cursos
 
 util.Cabecalho()
 cursos = util.InicializaArquivo()
@@ -24,7 +25,6 @@ print(cursos)
          description="Retornas todos os Curso",
          summary="Retorna todos dos cursos",
          response_model=Dict[int, Curso],
-        #  response_model=Curso,
          response_description="Cursos encontrados com sucesso!"
          ) 
 async def get_cursos():
@@ -37,10 +37,7 @@ async def get_cursos():
 async def get_curso(curso_id: int):
     int_keys = [int(key) for key in cursos.keys()]
     try:
-        if curso_id <= 19 :
-            curso = cursos[str(curso_id)]
-        else:
-            curso = cursos[curso_id]
+        curso = cursos[str(curso_id)]
         print(curso)
         return curso
     except KeyError:
@@ -49,15 +46,16 @@ async def get_curso(curso_id: int):
             detail=f"ID Curso id {curso_id} não encontrado..."
         )
         
-        
 @app.get('/cursos/titulo/{titulo}',
          description="Retorna todos os cursos cujo título começa com o nome fornecido",
          summary="Retorna cursos pelo primeiro nome do título")
 async def get_curso_por_titulo(titulo: str):
     # Filtra os cursos cujo título começa com o nome fornecido
     cursos_filtrados = {
-        curso_id: curso for curso_id, curso in cursos.items()
-        if curso["titulo"].lower().startswith(titulo.lower())
+        curso_id: 
+            curso 
+                for curso_id, curso in cursos.items()
+                    if curso["titulo"].lower().startswith(titulo.lower())
     }
     if not cursos_filtrados:
         raise HTTPException(
@@ -71,44 +69,47 @@ async def get_curso_por_titulo(titulo: str):
         description="Inclui um curso Novo no com ID Incremental",
         summary="Inclui um curso novo e seu ID")
 async def post_curso(curso : Curso):
-    if curso.id in cursos.keys():
+    if str(curso.id) in cursos.keys():
         print("ID Duplicado, dados não incluidos!")
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, 
                              detail=f"Já existe um curso com ID: {curso.id}.")
-    else:   
-        int_keys = [int(key) for key in cursos.keys()]
-    # Garante que o próximo ID seja sempre único
-        next_id = (max(int_keys)) + 1     
-        # Valida se o ID já existe e insere caso contrário
-        if  next_id not in cursos:
-            cursos[next_id] = curso
-            curso.id = next_id    
-            print(cursos)   
-            return curso
+    
+    # # Garante que o próximo ID seja sempre único
+    # Gera o próximo ID
+    int_keys = [int(k) for k in cursos.keys()]
+    next_id = max(int_keys) + 1 if int_keys else 1
+    
+    # Converte Curso para dicionario
+    curso_dict = curso.dict()
+    
+    # Adiciona o novo curso com a chave como str
+    curso_dict["id"] = next_id
+    cursos[str(next_id)] = curso_dict
+    print(cursos)
+    return curso
          
 @app.patch('/cursos/{curso_id}', 
         description="Atualiza dados do Curso pelo ID",
         summary="Atualiza dados do curso pelo ID")
 async def path_curso(curso_id: int, curso: AlteraCurso):
-    if curso_id in cursos:
-        curso_atualizado = cursos[curso_id]
+    if str(curso_id) in cursos:
+        curso_atualizado = cursos[str(curso_id)]
         print(curso)  
         curso.id = curso_id
-        cursos[curso_id] = curso
-        return cursos
+        cursos[str(curso_id)] = curso
+        return {"mensagem":f"Curso id {curso_id} atualizado com sucesso..."}
     else:
          raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"ID Curso id {curso_id} não encontrado..."
+            detail=f"Curso id {curso_id} não encontrado..."
         )
          
 @app.delete('/cursos/{curso_id}', 
          description="Exclui Curso pelo ID informado",
          summary="Remove um Curso pelo ID")
 async def del_curso(curso_id: int):
-    if curso_id in cursos:
-        del cursos[curso_id]
-        # return cursos
+    if str(curso_id) in cursos:
+        del cursos[str(curso_id)]
         return {"mensagem":f"Curso ID:{curso_id} removido com SUCESSO..."}
     else:
          raise HTTPException(
